f0fa59cf50d150536a3369fd7c47c6d5a2883549
ppazos <pablo.swp@gmail.com>
2021-10-11 20:16:02 -0300

    added parameters, json serialization of flat, and output to file

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # flat-tests
-Trasnformation from XML canonical compositions to a vendor specific FLAT format
+Trasnformation from Canonical XML openEHR COMPOSITIONs to a vendor specific FLAT format
 
 ## Build & Run
 $ mvn clean install
-$ java -cp target/flat-tests-0.0.1-jar-with-dependencies.jar org.ehrbase.flat_tests.Main
+$ java -Dcompo=path_to_xml_compo -Dopt=path_to_opt -cp target/flat-tests-0.0.1-jar-with-dependencies.jar org.ehrbase.flat_tests.Main
 
-## TODOs
-1. output json serialized flat
-2. input parameters
+## Sample call
+$ java -Dcompo=/home/pablo/Desktop/HiGHmed/minimal_observation.en.v1_20210929041240_000001_1.xml -Dopt=/home/pablo/Desktop/HiGHmed/minimal_observation.opt -cp target/flat-tests-0.0.1-jar-with-dependencies.jar org.ehrbase.flat_tests.Main
+
+## Output
+Will generate a file in the same path and with the same name es the -Dcompo value, with the trailing ".flat.json" added to it.
+
+## TODO
+
+- Transform Canonical JSON to FLAT
--- src/main/java/org/ehrbase/flat_tests/Main.java
+++ src/main/java/org/ehrbase/flat_tests/Main.java
@@ -20,6 +20,12 @@ import care.better.platform.web.template.converter.FromRawConversion;
 import care.better.platform.web.template.converter.flat.RawToFlatConverter;
 import care.better.platform.web.template.builder.context.WebTemplateBuilderContext;
 import javax.xml.transform.stream.StreamSource;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
+import care.better.platform.json.jackson.better.BetterObjectMapper;
 
 /**
  * @author Pablo Pazos <pablo.pazos@cabolabs.com>
@@ -27,18 +33,60 @@ import javax.xml.transform.stream.StreamSource;
  */
 public class Main
 {
+   static public File checkFile(String path)
+   {
+      File file = new File(path);
+
+      if (!file.exists())
+      {
+         System.out.println("File "+ path +" doesn't exist");
+         try {
+            System.out.println("File "+ file.getCanonicalPath() +" doesn't exist");
+         } catch (Exception e) {
+            System.out.println(e.getMessage());
+         }
+         System.exit(1);
+      }
+      
+      if (!file.canRead())
+      {
+         System.out.println("File "+ path +" foudn but can't be read, check permissions");
+         System.exit(1);
+      }
+
+      return file;
+   }
 
    /**
     * @param args
     */
    public static void main(String[] args)
    {
+      String compo_path = System.getProperty("compo");
+      String opt_path   = System.getProperty("opt");
+
+      if (compo_path == null)
+      {
+         System.out.println("Usage: $ java -Dcompo=path_to_xml_compo -Dopt=path_to_opt -cp target/flat-tests-0.0.1-jar-with-dependencies.jar org.ehrbase.flat_tests.Main");
+         System.out.println("-Dcompo=path_to_xml_compo is missing");
+         System.exit(1);
+      }
+
+      if (opt_path == null)
+      {
+         System.out.println("Usage: $ java -Dcompo=path_to_xml_compo -Dopt=path_to_opt -cp target/flat-tests-0.0.1-jar-with-dependencies.jar org.ehrbase.flat_tests.Main");
+         System.out.println("-Dopt=path_to_opt is missing");
+         System.exit(1);
+      }
+
+      File compo_file = checkFile(compo_path);
+      File opt_file   = checkFile(opt_path);
+
       try
       {
+         /* test read file from resources
          // read and parse compo
          String compositionFile = "minimal_evaluation.en.v1_20210927084720_000001_1.xml";
-         
-         Unmarshaller unmarshaller = JaxbRegistry.getInstance().getUnmarshaller();
          
          ClassLoader classLoader = Main.class.getClassLoader();
          
@@ -54,11 +102,15 @@ public class Main
             System.err.println("Composition resource was not found: "+ compositionFile);
             //throw new RuntimeException("Composition resource was not found: "+ compositionFile);
          }
+         */
+
+         FileInputStream inputStream = new FileInputStream(compo_file);
          
-         
+         Unmarshaller unmarshaller = JaxbRegistry.getInstance().getUnmarshaller();
          Composition compo = unmarshaller.unmarshal(new StreamSource(inputStream), Composition.class).getValue();
          
          
+         /*
          // read and parse template
          String optFile = "minimal_evaluation.opt";
          inputStream = classLoader.getResourceAsStream(optFile);
@@ -68,6 +120,9 @@ public class Main
             System.err.println("OPT resource was not found: "+ optFile);
             throw new RuntimeException("OPT resource was not found: "+ optFile);
          }
+         */
+
+         inputStream = new FileInputStream(opt_file);
          
          Template template = unmarshaller.unmarshal(new StreamSource(inputStream), Template.class).getValue();
          WebTemplate webTemplate = WebTemplateBuilder.buildNonNull(template, new WebTemplateBuilderContext("en", ImmutableList.of("en")));
@@ -75,11 +130,24 @@ public class Main
          
          Map<String, Object> flat = new RawToFlatConverter().convert(webTemplate, FromRawConversion.create(), compo);
          
+         /*
          for (Map.Entry<String, Object> e: flat.entrySet())
          {
             System.out.println(e.getKey() +": "+ e.getValue().toString());
          }
-         
+         */
+
+         // this outputs the json in a pretty format, without the writerWithDefaultPrettyPrinter() it is minified json.
+         BetterObjectMapper mapper = new BetterObjectMapper();
+         String prettyFormattedJson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(flat);
+
+         //System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(flat));
+
+         BufferedWriter writer = new BufferedWriter(new FileWriter(compo_file.getAbsolutePath() + ".flat.json")); // System.getProperty("file.separator") + 
+         writer.write(prettyFormattedJson);
+         writer.close();
+
+         System.out.println("Flat file generated: "+ compo_file.getAbsolutePath() + ".flat.json");
       }
       catch (Exception e)
       {
